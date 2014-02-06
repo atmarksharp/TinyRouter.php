41c85bb045028346b20faf564f3bf4e1133a4c41
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2014-02-06 17:43:00 +0900

    README updated

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-curl https://raw.github.com/atmarksharp/TinyRouter/master/tiny-router.php > tiny-router.php
-curl https://raw.github.com/atmarksharp/TinyRouter/master/.htaccess > .htaccess
+curl https://raw.github.com/atmarksharp/TinyRouter/master/tiny-router.php > tiny-router.php;
+curl https://raw.github.com/atmarksharp/TinyRouter/master/.htaccess > .htaccess;
