e09b52588095cd46cd03c10bf3a647343d61ca6f
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2014-02-06 19:43:37 +0900

    tiny-router.php updated

--- tiny-router.php
+++ tiny-router.php
@@ -1,58 +1,60 @@
 <?php
 
 class TinyRouter {
-	public $requestType;
-	public $params;
-	public $url;
-	public $query;
-	public $path;
-	private $_basePath;
+    public $requestType;
+    public $params;
+    public $url;
+    public $query;
+    public $path;
+    private $_basePath;
 
-	public function basePath($str = ''){
-		if($str){
-			$this->_basePath = $str;
-			$this->init();
-		}
+    public function basePath($str = ''){
+        if($str){
+            $this->_basePath = $str;
+            $this->init();
+        }
 
-		return $this->_basePath;
-	}
-	
-	private function init(){
-    	$bp = $this->_basePath;
-    	$pd = parse_url($this->url);
+        return $this->_basePath;
+    }
+    
+    private function init(){
+        $bp = $this->_basePath;
+        $pd = parse_url($this->url);
         $this->path = mb_ereg_replace("^$bp",'',$pd['path']);
-        $this->query = $pd['query'];
+        if(array_key_exists('query', $pd)){
+            $this->query = $pd['query'];
+        }
     }
 
-	public function __construct($basePath = ''){
-		global $_SERVER,$_GET,$_POST;
+    public function __construct($basePath = ''){
+        global $_SERVER,$_GET,$_POST;
 
-		$this->_basePath = $basePath;
+        $this->_basePath = $basePath;
         $this->requestType = $_SERVER['REQUEST_METHOD'];
         $this->url = $_SERVER['REQUEST_URI'];
         $this->params = ($this->requestType == 'GET')? $_GET : $_POST;
-   		$this->init();
+        $this->init();
     }
 
     public function response($method, $reg, $filepath){
-    	$req = $this->requestType;
-    	$path = $this->path;
-    	$params = $this->params;
-    	$mt = str_replace(' ','',$method);
+        $req = $this->requestType;
+        $path = $this->path;
+        $params = $this->params;
+        $mt = str_replace(' ','',$method);
 
-    	if($req == $mt || $mt == 'GET|POST' || $mt == 'POST|GET'){
-    		if(mb_ereg_match("^$reg$", $path)){
-    			$p = mb_ereg_replace("^$reg$",$filepath,$path);
-    			$parsed = parse_url($p);
-    			$newpath = $parsed['path'];
-    			parse_str($parsed['query'], $_GET);
-    			$this->renderPhp($newpath);
-    		}
-    	}
+        if($req == $mt || $mt == 'GET|POST' || $mt == 'POST|GET'){
+            if(mb_ereg_match("^$reg$", $path)){
+                $p = mb_ereg_replace("^$reg$",$filepath,$path);
+                $parsed = parse_url($p);
+                $newpath = $parsed['path'];
+                parse_str($parsed['query'], $_GET);
+                $this->renderPhp($newpath);
+            }
+        }
     }
 
     public function renderPhp($file){
-    	include $file;
+        include $file;
     }
 }
 ?>
