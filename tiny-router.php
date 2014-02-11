d6577cf1d35a3318f589cdae11e9a84939b5d97a
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2014-02-12 05:06:39 +0900

    tiny-router.php updated

--- tiny-router.php
+++ tiny-router.php
@@ -6,7 +6,28 @@ class TinyRouter {
     public $url;
     public $query;
     public $path;
+    public $notmatch = true;
+    private $_user;
+    private $_password;
     private $_basePath;
+
+    public function user($str = ''){
+        if($str){
+            $this->_user = $str;
+            $this->init();
+        }
+
+        return $this->_user;
+    }
+
+    public function password($str = ''){
+        if($str){
+            $this->_password = $str;
+            $this->init();
+        }
+
+        return $this->_password;
+    }
 
     public function basePath($str = ''){
         if($str){
@@ -44,11 +65,28 @@ class TinyRouter {
 
         if($req == $mt || $mt == 'GET|POST' || $mt == 'POST|GET'){
             if(mb_ereg_match("^$reg$", $path)){
+                $this->notmatch = false;
                 $p = mb_ereg_replace("^$reg$",$filepath,$path);
                 $parsed = parse_url($p);
                 $newpath = $parsed['path'];
                 parse_str($parsed['query'], $_GET);
                 $this->renderPhp($newpath);
+            }
+        }
+    }
+
+    public function responseAuth($method, $reg, $filepath){
+        if(!isset($_SERVER["PHP_AUTH_USER"])) {
+            header("WWW-Authenticate: Basic realm=\"Authorization Required\"");
+            header("HTTP/1.0 401 Unauthorized");
+
+            die("Authorization Required");
+        }
+        else {
+            if ($_SERVER['PHP_AUTH_USER'] == $this->_user && $_SERVER['PHP_AUTH_PW'] == $this->_password){
+                $this->response($method, $reg, $filepath);
+            }else {
+                die("Authorization Required");
             }
         }
     }
